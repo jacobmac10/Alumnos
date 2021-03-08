7c1f7e7a29a07eb9a64e5f3756b59050e2a88c78
Benjamin Macias <jacobmac10@hotmail.com>
2021-03-08 10:00:49 -0700

    ncluimos la clase Persona previamente trabajada. Creamos una clase Alumno [Student] que extienda de la clase Persona (herencia), agregando un campo matrícula

---
+++ Alumno.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Personas
+{
+    class Persona
+    {
+        public string nombre;
+        public string apellido;
+
+        // Método sin void/string/int/etc.
+        // Se llama igual que la clase
+        // Método constructor
+        public Persona(string nombre, string apellido)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+        public Alumno(string nombre, string apellido, int matricula)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.matricula = matricula;
+        }
+        public class Persona : Alumno
+        {
+            
+        }
+
+        public void Presentarse()
+        {
+            // Console.WriteLine("Hola, mi nombre es " + this.nombre + " " + this.apellido);
+            Console.WriteLine($"Hola mi nombre es {this.nombre} {this.apellido}");
+        }
+    }
+}
