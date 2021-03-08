bb6e164558753bf0b78670b344de8eeff4c91e3a
Benjamin Macias <jacobmac10@hotmail.com>
2021-03-08 10:52:23 -0700

    Crear una clase Materia [Subject], que tenga nombre y semestre

---
+++ Materia.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Materias
+{
+    class Materia
+    {
+        
+        public string nombre;
+        public string semestre;
+
+      
+        public Alumno(string nombre, string semestre)
+        {
+            this.nombre = nombre;
+            this.semestre = semestre;
+            
+        }
+       
+
+        
+    }
+}
